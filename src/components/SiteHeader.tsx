ce89f845fee80f559f5cb683e095767c182fc5b5
mimikyu <0203.sub@gmail.com>
2024-01-14 08:04:11 +0900

    modified:   src/components/SiteHeader.tsx

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -12,11 +12,11 @@ export function BetaIcon() {
         alignItems: 'center',
         color: '#000',
         position: 'relative',
-        height: '17px',
-        width: '17px',
+        height: '8px',
+        width: '8px',
       }}
     >
-      <SvgIcon sx={{ fill: '#777', fontSize: '14px', top: '6px', position: 'absolute' }}>
+      <SvgIcon sx={{ fill: '#777', fontSize: '14px', top: '-2px', left: '-6px', position: 'absolute' }}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 847 847" style={{ rotate: '17deg' }}>
           <path d="M284 796c9 15 5 34-10 43-15 10-34 5-44-10-17-29-42-247-36-451 3-133 18-262 54-335 31-62 290-60 323 56 14 49-15 87-55 109 77 28 148 86 137 176-19 149-209 194-332 173-17-3-29-20-26-37s20-29 37-26c85 15 244-8 258-117 14-115-232-134-263-133s-33-64 0-63c34 0 199-9 183-64-19-67-161-52-210-35-28 66-40 180-43 297-5 194 13 394 27 417z" />
         </svg>
@@ -27,9 +27,9 @@ export function BetaIcon() {
 
 export function SiteTitleBtn() {
   return (
-    <Button className="header_site_title" href="https://openchat-review.me" sx={{ p: 0 }}>
-      <img src="https://openchat-review.me/assets/icon-192x192.png" alt="" />
-      <div style={{ whiteSpace: 'nowrap' }}>
+    <Button href="https://openchat-review.me" sx={{ p: 0, display: 'flex' }}>
+      <div className="header_site_title">
+        <img src="https://openchat-review.me/assets/icon-192x192.png" alt="" />
         <h1>オプチャグラフ</h1>
         <BetaIcon />
       </div>
