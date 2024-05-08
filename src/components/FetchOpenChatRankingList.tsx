df39d0bc51378ce95c882f8e431daf2bd77063ab
mimikyu <0203.sub@gmail.com>
2024-05-08 22:24:44 +0900

    modified:   src/components/FetchOpenChatRankingList.tsx

--- src/components/FetchOpenChatRankingList.tsx
+++ src/components/FetchOpenChatRankingList.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useRef } from 'react'
+import React, { memo, useEffect, useRef } from 'react'
 import useInfiniteFetchApi from '../hooks/InfiniteFetchApi'
 import { useRecoilValue } from 'recoil'
 import { listParamsState } from '../store/atom'
