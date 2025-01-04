2977a6a1a4d19ccd7d984dc74a4dd4bb967f7303
mimimiku778 <0203.sub@gmail.com>
2025-01-05 08:22:33 +0900

    多言語対応のロジックを実装

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box, Button, SvgIcon } from '@mui/material'
 import SiteHeaderSearch from './SiteHeaderSearch'
-import { rankingArgDto } from '../config/config'
+import { rankingArgDto, siteUrl } from '../config/config'
 
 export function BetaIcon() {
   return (
@@ -17,9 +17,11 @@ export function BetaIcon() {
         width: '8px',
       }}
     >
-      <SvgIcon sx={{ fill: '#777', fontSize: '14px', top: '-2px', left: '-6px', position: 'absolute' }}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 847 847" style={{ rotate: '17deg' }}>
-          <path d="M284 796c9 15 5 34-10 43-15 10-34 5-44-10-17-29-42-247-36-451 3-133 18-262 54-335 31-62 290-60 323 56 14 49-15 87-55 109 77 28 148 86 137 176-19 149-209 194-332 173-17-3-29-20-26-37s20-29 37-26c85 15 244-8 258-117 14-115-232-134-263-133s-33-64 0-63c34 0 199-9 183-64-19-67-161-52-210-35-28 66-40 180-43 297-5 194 13 394 27 417z" />
+      <SvgIcon
+        sx={{ fill: '#777', fontSize: '14px', top: '-2px', left: '-6px', position: 'absolute' }}
+      >
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 847 847' style={{ rotate: '17deg' }}>
+          <path d='M284 796c9 15 5 34-10 43-15 10-34 5-44-10-17-29-42-247-36-451 3-133 18-262 54-335 31-62 290-60 323 56 14 49-15 87-55 109 77 28 148 86 137 176-19 149-209 194-332 173-17-3-29-20-26-37s20-29 37-26c85 15 244-8 258-117 14-115-232-134-263-133s-33-64 0-63c34 0 199-9 183-64-19-67-161-52-210-35-28 66-40 180-43 297-5 194 13 394 27 417z' />
         </svg>
       </SvgIcon>
     </span>
@@ -29,8 +31,8 @@ export function BetaIcon() {
 export function SiteTitleBtn() {
   return (
     <Button href={rankingArgDto.baseUrl} sx={{ p: 0, display: 'flex' }}>
-      <div className="header_site_title">
-        <img src={`${rankingArgDto.baseUrl}/assets/icon-192x192.png`} alt="" />
+      <div className='header_site_title'>
+        <img src={`${siteUrl}/assets/icon-192x192.png`} alt='' />
         <h1>オプチャグラフ</h1>
       </div>
     </Button>
@@ -47,7 +49,7 @@ export default function SiteHeader({
   siperSlideTo: (index: number) => void
 }) {
   return (
-    <div className="site_header_box" style={{ height }}>
+    <div className='site_header_box' style={{ height }}>
       <SiteHeaderSearch
         siperSlideTo={siperSlideTo}
         headerInnerStyle={{
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,34 +1,22 @@
-export const OPEN_CHAT_CATEGORY: [string, number][] = [
-  ['すべて', 0],
-  ['ゲーム', 17],
-  ['スポーツ', 16],
-  ['芸能人・有名人', 26],
-  ['同世代', 7],
-  ['アニメ・漫画', 22],
-  ['金融・ビジネス', 40],
-  ['音楽', 33],
-  ['地域・暮らし', 8],
-  ['ファッション・美容', 20],
-  ['イラスト', 41],
-  ['研究・学習', 11],
-  ['働き方・仕事', 5],
-  ['学校・同窓会', 2],
-  ['料理・グルメ', 12],
-  ['健康', 23],
-  ['団体', 6],
-  ['妊活・子育て', 28],
-  ['乗り物', 19],
-  ['写真', 37],
-  ['旅行', 18],
-  ['動物・ペット', 27],
-  ['TV・VOD', 24],
-  ['本', 29],
-  ['映画・舞台', 30],
-]
+export const rankingArgDto: RankingArgDto = JSON.parse(
+  document.getElementById('arg-dto')!.textContent!
+)
+
+export const OPEN_CHAT_CATEGORY: [string, number][] = rankingArgDto.openChatCategory
 
 export const OPEN_CHAT_CATEGORY_OBJ = ((obj) => {
-  OPEN_CHAT_CATEGORY.map((el) => obj[el[1]] = el[0])
+  OPEN_CHAT_CATEGORY.map((el) => (obj[el[1]] = el[0]))
   return obj
 })({} as { [K: string]: string })
 
-export const rankingArgDto: RankingArgDto = JSON.parse(document.getElementById('arg-dto')!.textContent!)
+export const basePath = `${
+  rankingArgDto.urlRoot ? rankingArgDto.urlRoot.replace(/^\/+/, '') + '/' : ''
+}ranking`
+
+const parsedUrl = new URL(rankingArgDto.baseUrl)
+export const siteUrl = `${parsedUrl.protocol}//${parsedUrl.hostname}${
+  parsedUrl.port ? ':' + parsedUrl.port : ''
+}`
+
+const langCode = rankingArgDto.urlRoot.replace(/^\/+/, '')
+export const imageBaseUrl = `${siteUrl}/oc-img${langCode ? '-' + langCode : ''}/`
